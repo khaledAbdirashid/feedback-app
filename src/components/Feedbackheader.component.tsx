065117256b80502885b6f1b2825eccfaf095a9aa
khaledAbdirashid <khaledabdirashid12@gmail.com>
2025-03-29 21:11:21 +0300

    added global styles and the header component

---
+++ src/components/Feedbackheader.component.tsx
@@ -0,0 +1,11 @@
+const FeedbackHeader = () => {
+  return (
+    <header className="feedback-header">
+      <div className="container">
+        <h1>Feedback ui</h1>
+      </div>
+    </header>
+  );
+};
+
+export default FeedbackHeader;
